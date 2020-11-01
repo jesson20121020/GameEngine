2167f4b8e7f3ef14801911a46cf6ea3cae0cc16f
xdcn4066 <xdcn4066@corp.netease.com>
2020-11-01 09:51:09 +0800

    添加渲染管理类，用于处理渲染器相关

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../Interface/IRuntimeModule.hpp"
+
+namespace My {
+class GraphicsManager : implements IRuntimeModule {
+
+public:
+  virtual ~GraphicsManager() {}
+};
+} // namespace My
