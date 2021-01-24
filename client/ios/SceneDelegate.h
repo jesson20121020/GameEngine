7e694b89b4481c170e091251f55be56773ca4340
xdcn4066 <xdcn4066@corp.netease.com>
2021-01-24 16:11:09 +0800

    添加IOS初始工程

---
+++ client/ios/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  client
+//
+//  Created by jesson on 2021/1/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ client/ios/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  client
+//
+//  Created by jesson on 2021/1/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
