0e812d0c86a60782734d9afcee15ba2aacbdf2ea
jesson20121020 <xuedengchang@163.com>
2021-02-08 18:00:49 +0800

    添加win32窗口应用初始代码，配置vs cmake参数“

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -2,12 +2,16 @@
 
 if (APPLE)
 	set(PLATFORM_OSX ON)
+elseif (WIN32)
+	set(PLATFORM_WINDOWS ON)
 endif()
 
 if (PLATFORM_IOS)
 	message("当前为IOS平台")
 	include(cmake/ios.cmake)
-elseif(PLATFORM_OSX)
+elseif (PLATFORM_OSX)
 	message("当前为OSX平台")
 	include(cmake/mac.cmake)
+elseif(PLATFORM_WINDOWS)
+	message("当前为Windows平台")
 endif()
