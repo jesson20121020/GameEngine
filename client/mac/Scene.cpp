6d9de0b9cd62388ec944b2dbf749b5764539c503
jesson <jesson@jessondeiMac.local>
2021-08-29 22:21:33 +0800

    添加着色器风状态代码

--- client/mac/Scene.cpp
+++ client/mac/Scene.cpp
@@ -6,21 +6,41 @@
 //
 
 #include "Scene.h"
+#include <OpenGL //gl.h>
 #include <OpenGL/OpenGL.h>
-#include <OpenGL//gl.h>
 #include <OpenGl/glu.h>
 
-
 void Init(float width, float height) {
-    glMatrixMode(GL_PROJECTION);
-    gluPerspective(50.0f, width / height, 0.1f, 1000.0f);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+  glMatrixMode(GL_PROJECTION);
+  gluPerspective(50.0f, width / height, 0.1f, 1000.0f);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 }
 
-void Renderer(){
-    glClearColor(0.1f, 0.4f, 0.7f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+void Renderer() {
+  glClearColor(0.1f, 0.4f, 0.7f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glFlush();
 }
 
+/*
+ * 你好，三角形
+ * */
+void OpenGL_Hello_Triangle() {
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+  char* vsShader = "
+#version 330 core
+	  layout (location 0) in vec3 aPos;
+  void main(){
+	  gl_Position =
+  }
+	  "
+
+
+}
---
+++ client/mac/Shader.cpp
@@ -0,0 +1,92 @@
+#include "Shader.h"
+
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
+  // 1. 从文件路径读取顶点、片段着色器代码
+  std::string vertexCode;
+  std::string fragmentCode;
+  std::ifstream vShaderFile;
+  std::ifstream fShaderFile;
+  // 保证ifstream可以抛出异常
+  vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+  try {
+    //打开文件
+    vShaderFile.open(vertexPath);
+    fShaderFile.open(fragmentPath);
+    std::stringstream vShaderStream, fShaderStream;
+
+    // 读取文件缓冲区内容到数据流中
+    vShaderStream << vShaderFile.rdbuf();
+    fShaderStream << fShaderFile.rdbuf();
+
+    // 关闭文件处理器
+    vShaderFile.close();
+    fShaderFile.close();
+
+    vertexCode = vShaderStream.str();
+    fragmentCode = fShaderStream.str();
+  } catch (std::ifstream::failure e) {
+    std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+  }
+
+  const char *vShaderCode = vertexCode.c_str();
+  const char *fShaderCode = fragmentCode.c_str();
+
+  // 2.编译
+  unsigned int vertex, fragment;
+  int success;
+  char infoLog[512];
+
+  // 顶点着色器
+  vertex = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex, 1, &vShaderCode, NULL);
+  glCompileShader(vertex);
+  // 打印错误信息
+  glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILE_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  // 片段着色器
+  fragment = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment, 1, &fShaderCode, NULL);
+  glCompileShader(fragment);
+  glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILE_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  // 着色器程序
+  ID = glCreateProgram();
+  glAttachShader(ID, vertex);
+  glAttachShader(ID, fragment);
+  glLinkProgram(ID);
+  glGetProgramiv(ID, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(ID, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::LINK_Failed\n" << infoLog << std::endl;
+  }
+
+  // 删除着色器
+  glDeleteShader(vertex);
+  glDeleteShader(fragment);
+}
+
+void Shader::use() { glUseProgram(ID); }
+
+void Shader::setBool(const std::string &name, bool value) const {
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+  glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
