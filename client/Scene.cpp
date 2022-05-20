7e21caf9455681dc4500c749ef4d94311c2622d8
xdcn4066 <xdcn4066@corp.netease.com>
2022-05-20 19:29:43 +0800

    add load texture sample

--- client/Scene.cpp
+++ client/Scene.cpp
@@ -7,8 +7,9 @@
 
 #include "Scene.h"
 #include "Shader.h"
-#include "glheader.h"
 #include "backends/imgui_impl_opengl3.h"
+#include "glheader.h"
+#include "stb_image.h"
 #ifdef _WINDOWS
 #include "backends/imgui_impl_win32.h"
 #endif
@@ -76,14 +77,162 @@ void Test_TriangleWithIndexs() {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  glBindVertexArray(0);
-
-  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   Shader shader("G:\\Projects\\GitLab\\GameEngine\\client\\res\\test001.vs",
                 "G:\\Projects\\GitLab\\GameEngine\\client\\res\\test001.ps");
+  shader.use();
+  glBindVertexArray(VAO);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+// 加载纹理
+void Test_LoadTexture() {
+  float vertices[] = {
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
+
+  int indexs[] = {0, 1, 3, 1, 2, 3};
+  unsigned int VBO, VAO, EBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexs), indexs, GL_STATIC_DRAW);
+
+  // position attributes
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // load a texture
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D,
+                texture); // all upcoming GL_TEXTURE_2D operations now have
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannel;
+  unsigned char *data =
+      stbi_load("H:\\GitLab\\GameEngine\\client\\res\\test.jpg", &width,
+                &height, &nrChannel, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  Shader shader("H:\\GitLab\\GameEngine\\client\\res\\test002.vs",
+                "H:\\GitLab\\GameEngine\\client\\res\\test002.ps");
+  shader.use();
+  glActiveTexture(GL_TEXTURE0); // 在绑定纹理之前先激活纹理单元
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glBindVertexArray(VAO);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void Test() {
+
+  float vertices[] = {
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  // texture coord attribute
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // load and create a texture
+  // -------------------------
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D,
+                texture); // all upcoming GL_TEXTURE_2D operations now have
+                          // effect on this texture object
+  // set the texture wrapping parameters
+  glTexParameteri(
+      GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+      GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannel;
+  // The FileSystem::getPath(...) is part of the GitHub repository so we can
+  // find files on any IDE/platform; replace it with your own image path.
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char *data =
+      stbi_load("H:\\GitLab\\GameEngine\\client\\res\\test.jpg", &width,
+                &height, &nrChannel, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture";
+  }
+  stbi_image_free(data);
+
+  // bind Texture
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  Shader shader("H:\\GitLab\\GameEngine\\client\\res\\test002.vs",
+                "H:\\GitLab\\GameEngine\\client\\res\\test002.ps");
   shader.use();
   glBindVertexArray(VAO);
   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
@@ -107,23 +256,21 @@ void Renderer() {
   ImGui_ImplWin32_NewFrame();
 #endif
 
-  if (ImGui::GetCurrentContext())
-  {
-      ImGui::NewFrame();
+  if (ImGui::GetCurrentContext()) {
+    ImGui::NewFrame();
 
-      ImGui::ShowDemoWindow();
+    ImGui::ShowDemoWindow();
 
-      ImGui::Render();
+    ImGui::Render();
   }
 
-  //Test_Init_DrawTriangle();
-  Test_TriangleWithIndexs();
+  // Test_Init_DrawTriangle();
+  // Test_TriangleWithIndexs();
+  Test_LoadTexture();
+  // Test();
   glFlush();
 
   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-void Destroy()
-{
-    ImGui_ImplOpenGL3_Shutdown();
-}
+void Destroy() { ImGui_ImplOpenGL3_Shutdown(); }
