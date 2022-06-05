29eb923d2b37b0b69a30820a135617c43859197f
xdcn4066 <xdcn4066@corp.netease.com>
2022-06-05 09:03:00 +0800

    添加windows平台相关处理

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## 1. Windows平台
 ```bash
     # 生成vs工程 
-	cmake.exe -G "Visual Studio 16 2019" -B build/windows .
+	cmake.exe -G "Visual Studio 16 2019" -B build/windows -T ClangCL -A x64 .
     # cmake编译
     cmake.exe --build build/windows --target client --config Debug
     # 运行
--- client/windows/client.cpp
+++ client/windows/client.cpp
@@ -56,8 +56,7 @@ BOOL InitOpenGLEnv(HWND hWnd) {
 void onBeginDraw()
 {
 	glClearColor(0.1f, 0.4f, 0.7f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void drawImGui()
@@ -106,6 +105,36 @@ void onPostDraw()
 }
 
 
+static const unsigned int s_combinetion_number_table[15][15] = {
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 3, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 4, 6, 4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 5, 10, 10, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 6, 15, 20, 15, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 7, 21, 35, 35, 21, 7, 1, 0, 0, 0, 0, 0, 0, 0 },
+    { 1, 8, 28, 56, 70, 56, 28, 8, 1, 0, 0, 0, 0, 0, 0 },
+    { 1, 9, 36, 84, 126, 126, 84, 36, 9, 1, 0, 0, 0, 0, 0 },
+    { 1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1, 0, 0, 0, 0 },
+    { 1, 11, 55, 165, 330, 462, 462, 330, 165, 55, 11, 1, 0, 0, 0 },
+    { 1, 12, 66, 220, 495, 792, 924, 792, 495, 220, 66, 12, 1, 0, 0 },
+    { 1, 13, 78, 286, 715, 1287, 1716, 1716, 1287, 715, 286, 78, 13, 1, 0 },
+    { 1, 14, 91, 364, 1001, 2002, 3003, 3432, 3003, 2002, 1001, 364, 91, 14, 1 }
+};
+
+/// <summary>
+/// 计算排列组合, 这里直接读表，提高计算速度（由编辑器端限制n的个数，防止这里越界）
+/// </summary>
+/// <param name="n"></param>
+/// <param name="k"></param>
+/// <returns></returns>
+unsigned long CalcCombinetionNumber(int n, int k)
+{
+    return s_combinetion_number_table[n][k];
+}
+
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
                       _In_ int nCmdShow) {
@@ -140,6 +169,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
   }
 
   FileUtils::getInstance()->addSearchPath(exeDir + "/Resources", true);
+
+  auto ret = CalcCombinetionNumber(10, 5);
 
   // 执行应用程序初始化:
   if (!InitInstance(hInstance, nCmdShow)) {
